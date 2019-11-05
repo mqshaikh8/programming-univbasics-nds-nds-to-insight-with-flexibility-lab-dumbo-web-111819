732efca1e267d26fb366d9d663a3722c4abc8c21
Mohammed shaikh <mqshaikh8@gmail.com>
2019-11-05 12:31:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,6 +46,7 @@ def movies_with_director_key(name, movies_collection)
    new_hash = {}
   while counter < 18
     new_hash << movie_with_director_name(name, movies_collection.to_i)
+    new_hash[:director] = movies_collection 
     counter += 1
   end
   return new_hash
